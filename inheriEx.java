83b6e76aee4f31d8acb2f799aa9cbdc95cd1ec1a
Akshay Gautam <akshaygautam010@gmail.com>
2021-03-25 19:39:25 +0530

    inheritance

---
+++ inheriEx.java
@@ -0,0 +1,37 @@
+class room
+{
+  int len,bre;
+  room(int l,int b)
+  {
+    len=l;
+    bre=b;
+  }
+  int area()
+  {
+    return len*bre;
+  }
+}
+
+class hall extends room //inheritance
+{
+  int height;
+  hall(int l,int b,int h)
+  {
+    super(l,b);
+    height=h;
+  }
+  int volume()
+  {
+    return len*bre*height;
+  }
+}
+
+class inheriEx
+{
+  public static void main(String[] args)
+  {
+    hall h1=new hall(10,15,10);
+    System.out.println("Area of Hall = "+h1.area());
+    System.out.println("Volume of Hall = "+h1.volume());
+  }
+}
